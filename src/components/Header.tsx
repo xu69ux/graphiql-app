a7c9f5fde7b8c7ee4350e8ad72a3e977da67afe6
xu <spiridonova.kseniya@gmail.com>
2023-12-08 14:52:41 +0600

    feat: add sticky header and language toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,73 @@
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IoEarthOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { Fade } from '../components';
 
 import '@styles/Header.css';
 
 export const Header = () => {
   const username = 'user';
-  const logOut = () => {
-    console.log('log out');
+  const navigate = useNavigate();
+  const [isDropdownOpen, toggleDropdown] = useState(false);
+  const [selectedLanguage, selectLanguage] = useState('english');
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setScrollPosition(window.pageYOffset);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const headerStyle: React.CSSProperties = {
+    background:
+      scrollPosition > 100
+        ? 'linear-gradient(90deg, var(--green), var(--blue), var(--dark))'
+        : 'linear-gradient(90deg, var(--dark), var(--blue), var(--green))',
+    transition: 'background 2s',
   };
 
   return (
-    <header className='header'>
-      <Link to='/' className='welcome-link'>
-        <div className='logo'>GraphQL IDE</div>
-      </Link>
+    <header style={headerStyle} className='header'>
+      <div onClick={() => navigate('/')} className='welcome-link'>
+        <div className='logo'>GraphiQL IDE</div>
+        <div
+          className='lang-menu'
+          onClick={() => toggleDropdown(!isDropdownOpen)}
+        >
+          <IoEarthOutline className='lang-icon' title='change language' />
+          <Fade show={isDropdownOpen}>
+            <div className='lang-dropdown'>
+              <Link
+                to='#english'
+                onClick={() => selectLanguage('english')}
+                className={selectedLanguage === 'english' ? 'selected' : ''}
+              >
+                eng
+              </Link>
+              <Link
+                to='#russian'
+                onClick={() => selectLanguage('russian')}
+                className={selectedLanguage === 'russian' ? 'selected' : ''}
+              >
+                rus
+              </Link>
+              <Link
+                to='#ukrainian'
+                onClick={() => selectLanguage('ukrainian')}
+                className={selectedLanguage === 'ukrainian' ? 'selected' : ''}
+              >
+                ukr
+              </Link>
+            </div>
+          </Fade>
+        </div>
+      </div>
       <nav>
         <div className='user'>
           <span>
             hello, <b>{username}</b>!
           </span>
-          <IoEarthOutline className='lang-icon' />
-          <button className='btn logout' onClick={logOut}>
-            Log out
-          </button>
         </div>
       </nav>
     </header>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { AuthForm } from './AuthForm';
 export { Header } from './Header';
 export { Footer } from './Footer';
+export { Fade } from './Fade';
