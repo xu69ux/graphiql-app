bd8da6e48ea28b4a87f2fe8494a09566e54317d3
xu <spiridonova.kseniya@gmail.com>
2023-12-10 18:51:10 +0600

    feat: add draft layout for graphiQL page, add vite in gitignore

--- src/pages/GraphiQLPage.tsx
+++ src/pages/GraphiQLPage.tsx
@@ -1,17 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  IoSettingsSharp,
+  IoFileTrayFullOutline,
+  IoAddSharp,
+  IoChevronUpOutline,
+} from 'react-icons/io5';
+
+import '@styles/GraphiQLPage.css';
+
 export const GraphiQLPage = () => {
+  const [lineNumbers, setLineNumbers] = useState('1');
+  const navigate = useNavigate();
+  const user = 'user';
+
+  const updateLineNumbers = (event) => {
+    const lines = event.target.value.split('\n').length;
+    setLineNumbers(Array.from({ length: lines }, (_, i) => i + 1).join('\n'));
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
+
+  useEffect(() => {
+    const textArea = document.getElementById('text-area');
+    if (textArea) textArea.focus();
+  }, []);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '200rem',
-        fontSize: '20px',
-        color: '#fff',
-        background: '#333',
-      }}
-    >
-      <h1>GraphiQL Page</h1>
+    <div className='container'>
+      <div className='sidebar'>
+        <IoFileTrayFullOutline className='sidebar-icon docs' />
+        <IoSettingsSharp className='sidebar-icon settings' />
+        <IoAddSharp className='sidebar-icon add' />
+      </div>
+      <div className='container code'>
+        <div className='editor'>
+          <div className='editor body'>
+            <pre id='line-numbers'>{lineNumbers}</pre>
+            <textarea
+              id='text-area'
+              onInput={updateLineNumbers}
+              autoFocus
+            ></textarea>
+          </div>
+          <div className='editor-footer'>
+            <div className='variables'>variables</div>
+            <div className='headers'>headers</div>
+            <IoChevronUpOutline className='editor-footer-icon arrow' />
+          </div>
+        </div>
+        <div className='viewer'></div>
+      </div>
     </div>
   );
 };
