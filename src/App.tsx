3b800158443204e22b8c1f2547a1e43860b2b629
xu <spiridonova.kseniya@gmail.com>
2023-12-14 22:35:13 +0600

    feat: install error-boundary-react, add it in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import { routes } from './routes';
 import { Footer, Header } from './components';
 import { ToastContainer } from 'react-toastify';
@@ -10,25 +11,27 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <div className='App'>
-      <LanguageProvider>
-        <Router>
-          <Header />
-          <Routes>{routes}</Routes>
-          <Footer />
-          <ToastContainer
-            position='top-center'
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover={false}
-            theme='light'
-          />
-        </Router>
-      </LanguageProvider>
+      <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+        <LanguageProvider>
+          <Router>
+            <Header />
+            <Routes>{routes}</Routes>
+            <Footer />
+            <ToastContainer
+              position='top-center'
+              autoClose={2000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover={false}
+              theme='light'
+            />
+          </Router>
+        </LanguageProvider>
+      </ErrorBoundary>
     </div>
   );
 }
