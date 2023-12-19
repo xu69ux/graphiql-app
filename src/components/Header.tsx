63a11d151c187570772fb6b3ce47b30250ddabed
xu69ux <spiridonova.kseniya@gmail.com>
2023-12-19 16:06:51 +0600

    fix: update scroll functionality and styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { fetchUserName } from '../services/api/fetchUserName';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useState, useEffect, useContext, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { translations } from '../contexts/translations';
 import { Fade } from '../components';
 import { auth, logout } from '../utils/firebase';
@@ -19,7 +19,8 @@ export const Header = () => {
   const [username, setUserName] = useState('');
   const [user, loading] = useAuthState(auth);
   const [isDropdownOpen, toggleDropdown] = useState(false);
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrolled, setScrolled] = useState(false);
+
   const showMessage = useShowMessage();
   const msg = useMsg();
   const languageContext = useContext(LanguageContext) || {
@@ -38,10 +39,26 @@ export const Header = () => {
   }, [user]);
 
   useEffect(() => {
-    const onScroll = () => setScrollPosition(window.pageYOffset);
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 100;
+      if (isScrolled !== scrolled) {
+        setScrolled(!scrolled);
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    fetchData();
+  }, [user, loading, fetchData]);
 
   useEffect(() => {
     if (loading) {
@@ -57,29 +74,21 @@ export const Header = () => {
     sessionStorage.removeItem('authInfo');
   };
 
-  useEffect(() => {
-    if (loading) {
-      return;
-    }
-    fetchData();
-  }, [user, loading, fetchData]);
-
-  const headerStyle: React.CSSProperties = {
-    background:
-      scrollPosition > 100
-        ? 'linear-gradient(90deg, var(--green), var(--blue), var(--dark))'
-        : 'linear-gradient(90deg, var(--dark), var(--blue), var(--green))',
-    transition: 'background 2s',
-  };
-
   const renderUser = () => {
     return (
       <nav>
-        <div className='user'>
+        <div className={`user ${scrolled ? 'scrolled' : ''}`}>
           {username && (
-            <span>
-              {translations[language]?.greeting}, {username}!
-            </span>
+            <>
+              <span>
+                {translations[language]?.greeting}, {username}!
+              </span>
+              <Link to='/graphiql' className='graphiql'>
+                <span>
+                  go to <b>IDE</b>
+                </span>
+              </Link>
+            </>
           )}
           <CustomButton
             className='btn btn-header btn-logout'
@@ -111,16 +120,22 @@ export const Header = () => {
   };
 
   return (
-    <header style={headerStyle} className='header'>
+    <header className={`header ${scrolled ? 'scrolled' : ''}`}>
       <div className='welcome-link'>
-        <div className='logo' onClick={() => navigate('/')}>
+        <div
+          className={`logo ${scrolled ? 'scrolled' : ''}`}
+          onClick={() => navigate('/')}
+        >
           GraphiQL IDE
         </div>
         <div
           className='lang-menu'
           onClick={() => toggleDropdown(!isDropdownOpen)}
         >
-          <IoEarthOutline className='lang-icon' title='change language' />
+          <IoEarthOutline
+            className={`lang-icon ${scrolled ? 'scrolled' : ''}`}
+            title='change language'
+          />
           <Fade show={isDropdownOpen}>
             <div className='lang-dropdown'>
               <button
