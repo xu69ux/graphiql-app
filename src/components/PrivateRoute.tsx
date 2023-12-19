b56cdc7e175f2a87b1a8058d2caf709f70b04f46
xu69ux <spiridonova.kseniya@gmail.com>
2023-12-19 16:39:28 +0600

    fix: authentication logic in PrivateRoute component

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
   const user = sessionStorage.getItem('authInfo');
 
-  return user ? <Outlet /> : <Navigate to='/' />;
+  return !user ? <Outlet /> : <Navigate to='/' />;
 };
 
 export default PrivateRoute;
