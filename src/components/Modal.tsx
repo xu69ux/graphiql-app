d26443562ac935b7dfb8af3d4952970c55491c5c
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-02 20:10:54 +0600

    feat: add tostify message

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { IoCloseOutline } from 'react-icons/io5';
+import useShowMessage from '../hooks/useShowMessage';
+import useMsg from '../hooks/useMsg';
 
 import '@styles/Modal.css';
 
@@ -11,9 +13,16 @@ interface IModalProps {
 }
 
 export const Modal: FC<IModalProps> = ({ isOpen, onClose }) => {
+  const msg = useMsg();
+  const showMessage = useShowMessage();
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   if (!isOpen) return null;
+
+  const handleLocalClear = () => {
+    localStorage.clear();
+    showMessage(msg.LOCAL_STORAGE_CLEAR_SUCCESS);
+  };
 
   return (
     <div className='modal-overlay'>
@@ -32,10 +41,7 @@ export const Modal: FC<IModalProps> = ({ isOpen, onClose }) => {
             </div>
             <div className='local'>
               <h4>Local Storage:</h4>
-              <button
-                className='local-btn'
-                onClick={() => localStorage.clear()}
-              >
+              <button className='local-btn' onClick={handleLocalClear}>
                 clear
               </button>
             </div>
